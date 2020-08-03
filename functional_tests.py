a750869b46e9834ed16a827fddb246b63789a29e
NikitaYatsuraPrograming <nikitayatsuraprograming@gmail.com>
2020-08-03 19:38:14 +0300

    update functional_tests, settings(add apps), lists/tests(write test)

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,41 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+import unittest
 
-assert 'Django' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+    """
+    Тест нового посетителя
+    """
+
+    def setUp(self):
+        """
+        Устаноовка
+        :return:
+        """
+
+        self.browser = webdriver.Firefox(
+            firefox_binary='/home/nikita/firefox/firefox'
+        )
+
+    def tearDown(self):
+        """
+        Демонтаж
+        :return:
+        """
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """
+        Текст: можно начать список и получить его позже
+        :return:
+        """
+
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
