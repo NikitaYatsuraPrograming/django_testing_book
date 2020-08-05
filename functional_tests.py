825e408a3510058792edd05395db3a1f2dffec66
NikitaYatsuraPrograming <nikitayatsuraprograming@gmail.com>
2020-08-05 17:49:57 +0300

    functional test checks if input

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import time
+import unittest
+
 
 import unittest
 
@@ -34,6 +39,26 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do lists', self.browser.title)
+        header_test = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_test)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Купить павлиньи перья')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+
         self.fail('Закончить тест')
 
 
