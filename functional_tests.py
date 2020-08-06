e2cef6571d713c03d96a4cf1fbbfcf7e2f8dde45
NikitaYatsuraPrograming <nikitayatsuraprograming@gmail.com>
2020-08-06 15:58:46 +0300

    wrote a function that checks the occurent of a test record in the table

--- functional_tests.py
+++ functional_tests.py
@@ -30,6 +30,17 @@ class NewVisitorTest(unittest.TestCase):
         """
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """
+        Проверка на вхождение строки в таблицу списка
+        :param row_text:
+        :return:
+        """
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """
         Текст: можно начать список и получить его позже
@@ -53,37 +64,14 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-
-        self.fail('Закончить тест')
-
-    def test_can_start_a_list_and_retrieve_it_later_two(self):
-        """
-        Текст: можно начать список и получить его позже
-        :return:
-        """
-
-        self.browser.get('http://localhost:8000')
-
-        self.assertIn('To-Do lists', self.browser.title)
-        header_test = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_test)
-
         inputbox = self.browser.find_element_by_id('id_new_item')
-
         inputbox.send_keys('Сделать мушку из павлиньих перьев')
 
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
         self.fail('Закончить тест')
 
