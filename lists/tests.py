d69b9af86c380efcab3db0118f88045de1953c9b
NikitaYatsuraPrograming <nikitayatsuraprograming@gmail.com>
2020-08-07 10:52:36 +0300

    created a model Item and wrote test which checks the save information in db

--- lists/tests.py
+++ lists/tests.py
@@ -2,26 +2,13 @@ from django.test import TestCase
 from django.urls import resolve
 
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
     """
     Тест домашней страницы
     """
-
-    def test_home_page_returns_correct_html(self):
-        """
-        Тест: домашняя страница возвращает правильный html
-        :return:
-        """
-
-        response = self.client.get('/')
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
-
-        self.assertTemplateUsed(response, 'lists/home_page.html')
 
     def test_user_home_template(self):
         """
@@ -39,3 +26,32 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'lists/home_page.html')
+
+
+class ItemModelTest(TestCase):
+    """
+    Тест: модели элемента списка
+    """
+
+    def test_saving_and_retrieving_items(self):
+        """
+        Тест: сохранение и получение элемента списка
+        :return:
+        """
+
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
