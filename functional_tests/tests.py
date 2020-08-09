62f34408c6677b8a463290804d83f56d529b9983
NikitaYatsuraPrograming <nikitayatsuraprograming@gmail.com>
2020-08-09 15:29:41 +0300

    corrected the mistake

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,8 +2,11 @@ from django.test import LiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 import time
+
+MAX_WAIT = 15
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -28,20 +31,27 @@ class NewVisitorTest(LiveServerTestCase):
         """
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
+    def wait_for_row_in_list_table(self, row_text):
         """
-        Проверка на вхождение строки в таблицу списка
+        Ожидать строку в таблице списка
         :param row_text:
         :return:
         """
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except(AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         """
-        Текст: можно начать список и получить его позже
+        Тест: можно начать список для одного пользователя
         :return:
         """
 
@@ -60,15 +70,63 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Купить павлиньи перья')
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Сделать мушку из павлиньих перьев')
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
-        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
         self.fail('Закончить тест')
+
+    def test_multiple_users_can_starts_lists_at_different_urls(self):
+        """
+        Тест: многочисленные пользователи могут начать список по разным url
+        :return:
+        """
+
+        self.browser.get(self.live_server_url)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+
+        # Получение уникального URL адресса
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # После мы искуственно создаем нового пользователя с полностью очищенным cookie
+        # используя новый сеанс
+        self.browser.quit()
+        self.browser = webdriver.Firefox(
+            firefox_binary='/home/nikita/firefox/firefox'
+        )
+
+        # Проходит проверка, что новый пользователь не видит списка других
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertNotIn('Сделать мушку из павлиньих перьев', page_text)
+
+        # Новый пользователь создает свой список дел
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Прочитать книгу')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: Прочитать книгу')
+
+        # Получение уникального URL адресса
+        new_users_list_url = self.browser.current_url
+        self.assertRegex(new_users_list_url, '/lists/.+')
+        self.assertNotEqual(new_users_list_url, edith_list_url)
+
+        # Проверка не содержит ли он чужой список
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertIn('Прочитать книгу', page_text)
