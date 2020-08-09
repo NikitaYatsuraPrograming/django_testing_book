e40a78e1c60136bac78b34b1eae26dc941f8bea3
NikitaYatsuraPrograming <nikitayatsuraprograming@gmail.com>
2020-08-09 11:02:05 +0300

    wrote test to check the list of tasks and save to db, updated views and home_page

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from lists.models import Item
 
 
 def home_page(request):
@@ -10,7 +12,10 @@ def home_page(request):
     :return:
     """
 
-    return render(request, 'lists/home_page.html',
-                  {
-                      'new_item_text': request.POST.get('item_text'),
-                  })
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+
+    return render(request, 'lists/home_page.html', {'items': items})
