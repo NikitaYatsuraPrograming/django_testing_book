e50a82a8452759637c9b50a30342db8a1c3115e8
NikitaYatsuraPrograming <nikitayatsuraprograming@gmail.com>
2020-08-10 16:44:42 +0300

    adding two remote tests

--- lists/tests.py
+++ lists/tests.py
@@ -117,6 +117,23 @@ class NewListTest(TestCase):
     Тест нового списка
     """
 
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        new_list = List.objects.first()
+        self.assertRedirects(response, quote(f'/lists/{new_list.pk}/'))
+
+
+class NewItemTest(TestCase):
+    """
+        Тест нового списка
+        """
     def test_can_save_a_POST_request_to_an_existing_list(self):
         """
         Тест: можно сохранить post-запрос в существующий список
